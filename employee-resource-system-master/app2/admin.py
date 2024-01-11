9a62236c97269ce08c0ce24339ab8b67e2cf67a9
BALAJI G M <109514220+balajigm292@users.noreply.github.com>
2024-01-11 11:57:50 +0530

    .

--- employee-resource-system-master/app2/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app2.models import *
-
-# Register your models here.
-
-admin.site.register(Department)
-admin.site.register(Position)
-admin.site.register(Employees)
